3921abc401f14bf1f3392f6dca00054b9101af26
Miguel-Lessa <162641574+Miguel-Lessa@users.noreply.github.com>
2024-04-15 08:44:28 -0300

    Classe cliente

---
+++ codigo/Cliente.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Cliente {
+    
+    private String nome;
+    private int id;
+    
+    public Cliente(String nome, int id){
+        this.nome = nome;
+        this.id = id;
+        
+    }
+    
+}
+
