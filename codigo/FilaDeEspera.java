5c8df56d939bbe735c573d0df08da1bf98635611
Cauan Augusto <130404379+cauan-au@users.noreply.github.com>
2024-04-15 22:47:32 -0300

    Update FilaDeEspera.java

--- codigo/FilaDeEspera.java
+++ codigo/FilaDeEspera.java
@@ -1,1 +1,35 @@
-public class FilaDeEspera
+public class FilaDeEspera {
+    
+    public FilaDeEspera() {
+        filaDeEspera = new Pedido[100];
+    }
+
+    public void adicionarPedido (Pedido pedido) {
+        int proximaPosicao = 0;
+
+        while (filaDeEspera[proximaPosicao] != null) {
+            proximaPosicao++;
+        }
+
+        filaDeEspera[proximaposicao] = pedido;
+    }
+
+    public void removerPedido () {
+        for (int i = 0; i < filaDeEspera.lenght; i++) {
+            if (filaDeEspera[i] == pedido) {
+                filaDeEspera[i] = null;
+                return;
+            }
+        }
+    }
+
+    public void consultarProximoCliente () {
+        for (Pedido : filaDeEspera) {
+            if (pedido != null) {
+                return pedido
+            }
+        }
+
+        return null;
+    }
+}
