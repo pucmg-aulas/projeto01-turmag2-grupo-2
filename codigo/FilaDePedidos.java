a36a9321f56c07f040a0de55559e6a4ad864af8d
Viihctor <131902065+Viihctor@users.noreply.github.com>
2024-04-22 22:52:35 -0300

    Update and rename FilaDeEspera.java to FilaDePedidos.java
    Mudança no nome da classe para nome melhor explicativo

---
+++ codigo/FilaDePedidos.java
@@ -0,0 +1,52 @@
+package sistemarestaurante;
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author victo
+ */
+public class FilaDePedidos {
+
+    ArrayList<Pedido> filaDeEspera;
+
+    public FilaDePedidos() {
+        filaDeEspera = new ArrayList<Pedido>();
+
+    }
+
+    public void adicionarPedidoAFila(Pedido pedido) {
+        filaDeEspera.add(pedido);
+    }
+
+    public void removerPedidoDaFila(int id) {
+        filaDeEspera.remove(id);
+    }
+
+    public Pedido consultarProximoCliente(int capacidade) {
+        for (Pedido pedido : filaDeEspera) {
+            if (pedido != null) {
+                if (pedido.numeroDePessoas <= capacidade) {
+                    return pedido;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public void listarPedidos() {
+        for (Pedido pedido : filaDeEspera) {
+            System.out.println("----------------------------------------------------------------------------------------------");
+            System.out.println("Cliente: " + pedido.cliente.nome + " ID do pedido: " + pedido.id + " Num de Pessoas: " + pedido.numeroDePessoas + " Tel. P/ contato: " + pedido.cliente.telefone);
+            System.out.println("Dia Entrada: " + pedido.horaEntrada + " Dia Saida: " + pedido.horaSaida + " Hora da Entrada: " + pedido.horaEntrada + " Hora da Saida: " + pedido.horaSaida);
+            System.out.println("----------------------------------------------------------------------------------------------");
+        }
+
+    }
+
+}
