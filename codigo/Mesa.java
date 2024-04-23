e432bea621b365c0eba4577db6a7574980731be2
Viihctor <131902065+Viihctor@users.noreply.github.com>
2024-04-22 22:51:37 -0300

    Update Mesa.java
    Versão final Mesa

--- codigo/Mesa.java
+++ codigo/Mesa.java
@@ -1,52 +1,36 @@
-import java.util.*;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemarestaurante;
 
+/**
+ *
+ * @author victo
+ */
+import java.util.*;
 public class Mesa {
     int idMesa;
     int capacidade;
     boolean disponibilidade;
-
-    public Mesa(int idMesa, int capacidade, boolean disponibilidade) {
+    public Mesa(int idMesa, int capacidade, boolean disponibilidade){
         this.idMesa = idMesa;
         this.capacidade = capacidade;
         this.disponibilidade = disponibilidade;
     }
 
-    public int verificarDisponiblidade(Mesa mesas[]) {
-        for (int i = 0; i < mesas.length; i++) {
-            if (mesas[i].disponibilidade) {
-                return mesas[i].idMesa;
-            }
-        }
-        return -1;
-    }
-
-    public void alocarMesa(int idPedido) {
+    public void alocarMesa(int idPedido){
         this.disponibilidade = false;
     }
-
-    public void desalocarMesa() {
+    public void desalocarMesa(){
         this.disponibilidade = true;
     }
-
-    public boolean verificarCapacidade(Pedido pedido) {
-        int pessoas = pedido.numeroPessoas;
-        if (this.capacidade >= pessoas) {
+    public boolean verificarCapacidade(Pedido pedido){
+        int pessoas = pedido.numeroDePessoas;
+        if(this.capacidade >= pessoas){
             return true;
         }
         return false;
     }
-
-    public void listarMesasDisponiveis(Mesa mesas[]) {
-        System.out.println("Mesas Disponiveis:");
-        for (int i = 0; i < mesas.length; i++) {
-            if (mesas[i].disponibilidade) {
-                System.out.println(mesas[i].idMesa);
-            }
-        }
-    }
-
-    public double calcularTempoPermanencia(Cliente cliente) {
-
-    }
-
+   
 }
